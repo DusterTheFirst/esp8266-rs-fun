b3b952337a0558d27f7a5d7f50b1a9c6e9900cc6
Zachary Kohnen <me@dusterthefirst.com>
2021-10-09 14:55:47 +0200

    Debounce

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
 #![no_std]
 #![no_main]
 
-use esp8266_hal::{gpio::*, prelude::*, target::Peripherals};
+use core::fmt::Write;
+use esp8266_hal::{
+    gpio::*,
+    interrupt::{enable_interrupt, InterruptType},
+    prelude::*,
+    target::Peripherals,
+    uart::UART0Serial,
+};
 use panic_halt as _;
 use xtensa_lx::mutex::{CriticalSectionMutex, Mutex};
+
+use crate::time::{current_millis, initialize_timekeeping};
+
+mod time;
 
 #[entry]
 fn start() -> ! {
     main();
 }
 
-static BUZZER: CriticalSectionMutex<Option<Gpio4<Output<PushPull>>>> =
+#[interrupt]
+fn gpio() {
+    gpio_debounce();
+}
+
+struct GpioInterruptData {
+    buzzer: Gpio4<Output<PushPull>>,
+    red_led: Gpio5<Output<PushPull>>,
+    button1: Gpio14<Input<PullUp>>,
+    button2: Gpio12<Input<PullUp>>,
+}
+
+static INTERRUPT_DATA: CriticalSectionMutex<Option<GpioInterruptData>> =
     CriticalSectionMutex::new(None);
-static RED_LED: CriticalSectionMutex<Option<Gpio5<Output<PushPull>>>> =
-    CriticalSectionMutex::new(None);
+
+static SERIAL: CriticalSectionMutex<Option<UART0Serial>> = CriticalSectionMutex::new(None);
 
 fn main() -> ! {
     let dp = Peripherals::take().unwrap();
 
     let pins = dp.GPIO.split();
+
+    let mut serial = dp
+        .UART0
+        .serial(pins.gpio1.into_uart(), pins.gpio3.into_uart());
+
+    write!(serial, "\r\nInitialized:\r\n").unwrap();
 
     let mut builtin_led = pins.gpio2.into_push_pull_output();
     let mut red_led = pins.gpio5.into_push_pull_output();
@@ -27,34 +56,74 @@ fn main() -> ! {
     let mut button1 = pins.gpio14.into_pull_up_input();
     let mut button2 = pins.gpio12.into_pull_up_input();
 
-    button1.set_interrupt_mode(InterruptMode::BothEdges);
+    button1.set_interrupt_mode(InterruptMode::NegativeEdge);
+    button2.set_interrupt_mode(InterruptMode::NegativeEdge);
+    enable_interrupt(InterruptType::GPIO);
 
-    let (mut timer1, _) = dp.TIMER.timers();
+    let (timer1, mut timer2) = dp.TIMER.timers();
+
+    initialize_timekeeping(timer1);
 
     builtin_led.set_high().unwrap();
     red_led.set_high().unwrap();
     buzzer.set_low().unwrap();
 
-    (&BUZZER).lock(|buzzer_lock| *buzzer_lock = Some(buzzer));
-    (&RED_LED).lock(|led_lock| *led_lock = Some(red_led));
+    (&INTERRUPT_DATA).lock(|lock| {
+        *lock = Some(GpioInterruptData {
+            buzzer,
+            red_led,
+            button1,
+            button2,
+        })
+    });
+
+    (&SERIAL).lock(|serial_lock| *serial_lock = Some(serial));
 
     loop {
-        timer1.delay_ms(100);
+        timer2.delay_ms(500);
         builtin_led.toggle().unwrap();
+
+        (&SERIAL)
+            .lock(|serial_lock| {
+                writeln!(
+                    serial_lock.as_mut().unwrap(),
+                    "Current ms: {}",
+                    current_millis()
+                )
+            })
+            .unwrap();
         // red_led.toggle().unwrap();
     }
 }
 
-#[interrupt(GPIO)]
+fn gpio_debounce() {
+    static NEXT_VALID: CriticalSectionMutex<u128> = CriticalSectionMutex::new(0);
+    const INTERVAL: u128 = 500;
+
+    let millis = current_millis();
+
+    if millis > (&NEXT_VALID).lock(|f| *f) {
+        (&NEXT_VALID).lock(|f| *f = millis + INTERVAL);
+
+        button_interrupt();
+    }
+}
+
 fn button_interrupt() {
-    (&BUZZER).lock(|buzzer_lock| {
-        let buzzer = buzzer_lock.as_mut().unwrap();
+    (&SERIAL)
+        .lock(|serial| write!(serial.as_mut().unwrap(), "INTERRUPT"))
+        .unwrap();
 
-        buzzer.toggle().unwrap();
-    });
+    (&INTERRUPT_DATA).lock(|lock| {
+        let GpioInterruptData {
+            buzzer,
+            red_led,
+            button1,
+            button2,
+        } = lock.as_mut().unwrap();
 
-    (&RED_LED).lock(|led_lock| {
-        let red_led = led_lock.as_mut().unwrap();
+        button1.clear_interrupt();
+        button2.clear_interrupt();
 
         red_led.toggle().unwrap();
     });
---
+++ src/time.rs
@@ -0,0 +1,25 @@
+use esp8266_hal::{
+    interrupt::{enable_interrupt, InterruptType},
+    prelude::*,
+    timer::Timer1,
+};
+use xtensa_lx::mutex::{CriticalSectionMutex, Mutex};
+
+static CURRENT_MILLIS: CriticalSectionMutex<u128> = CriticalSectionMutex::new(0);
+
+pub fn initialize_timekeeping(mut timer: Timer1) {
+    enable_interrupt(InterruptType::TIMER1);
+
+    timer.enable_interrupts();
+    timer.start(1.ms());
+}
+
+#[interrupt]
+fn timer1() {
+    (&CURRENT_MILLIS).lock(|millis| *millis += 1);
+}
+
+#[must_use]
+pub fn current_millis() -> u128 {
+    (&CURRENT_MILLIS).lock(|millis| *millis)
+}
